c614c4b012c5dcd46785606b2a14690e749d75ca
wenhaoli <wenhaoli@stu.xmu.edu.cn>
2025-08-22 17:17:50 +0800

    update

---
+++ scripts/longbench.sh
@@ -0,0 +1,14 @@
+model=$1
+task=$2
+attn_pattern=$3
+sparsity=$4
+
+
+srun -p Intern5 --quotatype spot -N1 -n1 --gpus-per-task 1 \
+python -u eval/LongBench/pred.py \
+    --model $model --task $task \
+    --method streaming \
+    --attn_load_dir ${attn_pattern} \
+    --sparsity $sparsity \
+    --sink_size 64 \
+    --recent_size 256
---
+++ scripts/run_longbench.sh
@@ -0,0 +1,21 @@
+attn_pattern_name="lr=0.02-reg=0.05-ctx=1000_32000-multi_passkey10"
+models="Meta-Llama-3.1-8B-Instruct"
+
+# sparsities="0 0.5 0.75"
+sparsities="0"
+
+# tasks="samsum narrativeqa qasper triviaqa hotpotqa multifieldqa_en multifieldqa_zh 2wikimqa musique dureader gov_report qmsum multi_news vcsum trec lsht passage_count passage_retrieval_en passage_retrieval_zh lcc repobench-p"
+tasks="hotpotqa"
+
+for model in $models; do
+    for task in $tasks; do
+        for sparsity in $sparsities; do
+            bash scripts/longbench.sh $model $task "attn_patterns/${model}/${attn_pattern_name}" $sparsity
+        done
+    done
+done
+
+cd eval/LongBench
+for model in $models; do
+    python -u eval.py --model $model &
+done
